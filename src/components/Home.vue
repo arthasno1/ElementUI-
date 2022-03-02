54f1137b24218bc251d3b207cddabcee6662d1cd
arthasno1 <arthasno1@163.com>
2022-03-02 14:35:56 +0800

    完成了登录

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <el-button type="info" @click="login">退出</el-button>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+
+    data() {
+        return {}
+    },
+
+    methods: {
+        login() {
+            sessionStorage.clear('token')
+            this.$router.push('/login')
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
