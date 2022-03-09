51568ee90b67812ed6bf4ab418cf79d24513dbd1
arthasno1 <arthasno1@163.com>
2022-03-10 02:47:40 +0800

    完成商品分类开发

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import Welcome from '../components/Welcome.vue'
 import Users from '../components/Users.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
-import Cate from '../components/Cate.vue'
+import Cate from '../components/goods/Cate.vue'
 Vue.use(VueRouter)
 const router = new VueRouter({
     routes: [
